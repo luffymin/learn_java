c557d634a007a2b688081d4d3f60f05fa7e4cfaf
wangjianzheng <wangjianzheng@xhqb.com>
2021-09-23 21:33:03 +0800

    fix: run

--- netty/echo_server/src/main/java/com/example/echo_server/EchoServerApplication.java
+++ netty/echo_server/src/main/java/com/example/echo_server/EchoServerApplication.java
@@ -1,20 +1,22 @@
 package com.example.echo_server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
-public class EchoServerApplication {
+public class EchoServerApplication implements CommandLineRunner {
 
 	@Value("${server.port}")
- 	private int port;
+	private int port;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EchoServerApplication.class, args);
 	}
 
-	@Override
-    public void run(String... args) {
-        logger.info(String.format("%s", String.valueOf(port)));
+    public void run(String... args) throws Exception {
+		
+    	new EchoServer(port).start();
     }
 }
