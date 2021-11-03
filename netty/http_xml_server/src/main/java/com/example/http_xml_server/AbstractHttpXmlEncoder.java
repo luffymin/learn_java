a4917d881310a0d7cf6d96cc50796e26f9220061
luffymin <wangjianzheng@xhqb.com>
2021-11-03 19:41:27 +0800

    init http xml server

--- netty/http_xml_server/src/main/java/com/example/http_xml_server/AbstractHttpXmlEncoder.java
+++ netty/http_xml_server/src/main/java/com/example/http_xml_server/AbstractHttpXmlEncoder.java
@@ -1,4 +1,18 @@
 package com.example.http_xml_server;
 
-public @interface AbstractHttpXmlEncoder {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import java.beans.XMLEncoder;
+import java.io.ByteArrayOutputStream;
+
+public abstract class AbstractHttpXmlEncoder<T> extends MessageToMessageEncoder<T> {
+
+    protected ByteBuf encode0(ChannelHandlerContext ctx, Object body) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        XMLEncoder encoder = new XMLEncoder(out);
+        encoder.writeObject(body);
+        return Unpooled.copiedBuffer(out.toByteArray());
+    }
 }
--- netty/http_xml_server/src/main/java/com/example/http_xml_server/HttpXmlResponse.java
+++ netty/http_xml_server/src/main/java/com/example/http_xml_server/HttpXmlResponse.java
@@ -1,4 +1,31 @@
 package com.example.http_xml_server;
 
+import io.netty.handler.codec.http.FullHttpResponse;
+
 public class HttpXmlResponse {
+
+    private FullHttpResponse httpResponse;
+    private Object result;
+
+    public HttpXmlResponse (FullHttpResponse httpResponse, Object result) {
+        this.httpResponse = httpResponse;
+        this.result = result;
+    }
+
+    public final FullHttpResponse getHttpResponse() {
+        return httpResponse;
+    }
+
+    public final void setHttpResponse (FullHttpResponse httpResponse) {
+        this.httpResponse = httpResponse;
+    }
+
+
+    public final Object getResult() {
+        return result;
+    }
+
+    public final void setResult (Object result) {
+        this.result = result;
+    }
 }
